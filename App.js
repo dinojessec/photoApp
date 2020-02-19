661f6c850ab5727d8f892c60a12af36e3d984bfb
Jesse Dino <jesse.dino.z3@gmail.com>
2020-02-19 16:22:33 +0800

    testing commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>DOT photo App</Text>
-      <Text>JESSE</Text>
+      <Text>JES1111111</Text>
     </View>
   );
 }
